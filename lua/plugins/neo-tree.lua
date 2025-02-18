924aefb5db807af4e6dfbfde884c8c98f6788582
Angel Vasilev <angavasilev@gmail.com>
2025-02-18 08:41:49 +0200

    my config

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,18 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  keys = {
+    {
+      '<leader>fe',
+      function()
+        require('neo-tree.command').execute { toggle = true, dir = vim.uv.cwd() }
+      end,
+      desc = 'Explorer NeoTree (cwd)',
+    },
+  },
+}
