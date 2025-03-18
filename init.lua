073ee6051e196100a030d71de91b0fe6a410aa3b
Angel Vasilev <angel.vasilev@halodx.com>
2025-03-18 16:39:28 +0200

    add bufferline

--- init.lua
+++ init.lua
@@ -209,6 +209,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 vim.wo.relativenumber = true
+vim.opt.termguicolors = true
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
